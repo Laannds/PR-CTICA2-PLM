c89f77531e17edad5b84be4062fd8f8ad949ab38
Laannds <landskron2020@gmail.com>
2022-10-27 10:21:19 +0200

    HASTA EL 3

---
+++ continua3.js
@@ -0,0 +1,21 @@
+let listaContactos =  [ {nombre : "Maxwell Wright",telefono: "(0191)7196495",correo : "Curabitur.egestas.nun@nonummyac.co.uk"},
+                        {nombre : "Raja Villareal",telefono : "0866 398 2895",correo : "posuere.vulputate@sed.com"},
+                        {nombre : "Helen Richards ", telefono : "0800 1111", correo : "libero@convalis.edu"}
+                    ];
+
+listaContactos.push ({nombre : "Maisie Haley", telefono : "0913 531 3030", correo : "risus.Quisque@urna.ca"});
+//new listaContactos("Maisie Haley","0913 531 3030","risus.Quisque@urna.ca");
+let nombreIntroducido = prompt("Introduce el nombre") ; 
+let telefonoIntroducido = prompt("Introduce el telefono");
+let correoTelefono = prompt("Introduce correo");
+listaContactos.push({nombre : nombreIntroducido , telefono : telefonoIntroducido , correo : correoTelefono} );
+
+
+var i = 0 ; 
+for (const iterator of listaContactos) {
+    if(i == 0 || i == listaContactos.length-1)
+    {
+        console.log(iterator)
+    }
+    i = i + 1 ; 
+}
